d962ee2782be7a5bb79607efc174dba1e5c0b26c
Mouse <m2k1661994@gmail.com>
2020-03-05 22:28:13 +0630

    add valitation to the input

--- src/coder/myo/Main.java
+++ src/coder/myo/Main.java
@@ -7,15 +7,33 @@ public class Main {
     public static void main(String[] args) {
         byte MONTHS_IN_YEARS = 12;
         byte PERCENT = 100;
+        int principal;
+        float monthlyInterset;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.println("Principal ($1K - 1M):  ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal < 1000000){
+                break;
+            }else {
+                System.out.println("Enter a number Between 1,000 and 1,000,000");
+            }
+        }
 
-        System.out.println("Annual Interest Rate: " );
-        float annualInterset = scanner.nextFloat();
-        float monthlyInterset = annualInterset / PERCENT / MONTHS_IN_YEARS;
+        while (true) {
+            System.out.println("Annual Interest Rate: ");
+            float annualInterset = scanner.nextFloat();
+            if(annualInterset >= 1 && annualInterset <= 30){
+                monthlyInterset = annualInterset / PERCENT / MONTHS_IN_YEARS;
+                break;
+            }else {
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+            }
+        }
+
+
 
         System.out.println("Period (Year)");
         byte years = scanner.nextByte();
