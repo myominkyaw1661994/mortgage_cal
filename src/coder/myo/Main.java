9d497a7425766d4829a06bdedee1aeffe022a3fc
Mouse <m2k1661994@gmail.com>
2020-03-06 16:56:16 +0630

    After refactoring the code.

--- src/coder/myo/Main.java
+++ src/coder/myo/Main.java
@@ -4,47 +4,40 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        byte MONTHS_IN_YEARS = 12;
-        byte PERCENT = 100;
-        int principal;
-        float monthlyInterset;
+        int principal = (int)readNumber("Principal ($1K - 1M):  ", 1000, 1000000);
+        float annualInterset = (float)readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) readNumber("Period (Year)", 1 , 30);
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Principal ($1K - 1M):  ");
-            principal = scanner.nextInt();
-            if(principal >= 1000 && principal < 1000000){
-                break;
-            }else {
-                System.out.println("Enter a number Between 1,000 and 1,000,000");
-            }
-        }
-
-        while (true) {
-            System.out.println("Annual Interest Rate: ");
-            float annualInterset = scanner.nextFloat();
-            if(annualInterset >= 1 && annualInterset <= 30){
-                monthlyInterset = annualInterset / PERCENT / MONTHS_IN_YEARS;
-                break;
-            }else {
-                System.out.println("Enter a value greater than 0 and less than or equal to 30");
-            }
-        }
-
-
-
-        System.out.println("Period (Year)");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEARS;
-
-        double mortgage = principal * (monthlyInterset * Math.pow(1 + monthlyInterset, numberOfPayments))
-                / (Math.pow(1 + monthlyInterset, numberOfPayments) - 1);
+        double mortgage = calculateMortgage(principal, annualInterset, years);
 
         String mortgageFomatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage :"+ mortgageFomatted);
+    }
+
+    private static double readNumber(String prompt, double min, double max){
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while(true) {
+            System.out.println(prompt);
+            value = scanner.nextDouble();
+            if(value >= min && value <= max) {
+                break;
+            }
+            System.out.println("Enter value between "+ min+" and " + max +" .");
+        }
+        return value;
+    }
+
+    private static double calculateMortgage(int principal, float annualInterset, byte years){
+        byte MONTHS_IN_YEARS = 12;
+        byte PERCENT = 100;
+        short numberOfPayments = (short)(years * MONTHS_IN_YEARS);
+        float monthlyInterset = annualInterset / PERCENT / MONTHS_IN_YEARS;
 
 
+        return principal * (monthlyInterset * Math.pow(1 + monthlyInterset, numberOfPayments))
+                / (Math.pow(1 + monthlyInterset, numberOfPayments) - 1);
     }
 }
